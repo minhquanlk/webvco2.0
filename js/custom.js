9eed10f7bea01fb99054ea7bfb205f7be242655b
minhquanlk <minhquanlk20022@gmail.com>
2024-01-29 18:40:13 +0700

    viww

--- js/custom.js
+++ js/custom.js
@@ -1601,3 +1601,32 @@ document.querySelectorAll('.career-card').forEach(function(div) {
         }, 650);
     });
 });
+var countDownDate = new Date("February 5, 2024 23:59:59").getTime();
+
+// Update the count down every 1 second
+var countdownfunction = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in elements with id="days", "hours", "minutes" and "seconds"
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("countdown1").innerHTML = "EXPIRED";
+    }
+}, 1000);
